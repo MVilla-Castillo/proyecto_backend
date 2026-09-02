4ec85e2a776a07a93c768a85100ce6af66da897f
Marcos Castillo <castillo.villa3@gmail.com>
2026-09-02 19:36:27 -0400

    commit final

--- cowork/views.py
+++ cowork/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'cowork/index.html')
+
+def error_404(request):
+    return render(request, 'templates/404.html')
