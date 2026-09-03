56ec44c10adac988916e5c237c71571a658e6259
Marcos Castillo <castillo.villa3@gmail.com>
2026-09-02 20:16:56 -0400

    ultimos ajustes

--- config/urls.py
+++ config/urls.py
@@ -3,3 +3,5 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('cowork.urls')),
 ]
+
+handler404 = 'cowork.views.error_404'
--- cowork/views.py
+++ cowork/views.py
@@ -4,5 +4,8 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'cowork/index.html')
 
-def error_404(request):
-    return render(request, 'templates/404.html')
+def error_404(request, exception):
+    return render(request, '404.html')
+
+def error_400(request, exception):
+    return render(request, '400.html')
